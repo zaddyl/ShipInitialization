c24baa5f8b2b7d3d1b1013ad93c6c04ac5d3af32
政道 李 <addylee0929@gmail.com>
2024-07-22 16:27:14 +0800

    setBlock() func
    trying to make it clearly.

--- script.cs
+++ script.cs
@@ -1,95 +1,28 @@
 //Grid Initialization
-//This script will auto-save specified entitys to customdata for calling;
-//Inludes thuster, battery, o2tank, h2tank, block that has inventory, antenna, beacon, power-generator, gas-generator, gyro
-//plz enter follow module's name after first run:
-//1.The connector for parking
-//2.The timer-block for counting down
-//Then set timer-block's action to run this pb with argument "Start"
-//And if run this script with "DockOn", it'll set battery to "Recharge", thruster to "turn-off", beacon to "turn-off", gyro to "turn-off" and vice versa for argument "Dockoff".
-//DO NOT MOVE your grid when initializing
-//Script will disconnect all of the connector then re-connect after 1 sec
+//This script will save the entitys in dictionary to customdata when press running;
+//Only works for CURRENT GRID, plz place the PB on which's initialization needed;
+//Includes thruster, battery, o2tank, h2tank, block that has inventory, antenna, beacon, power-generator, gas-generator, gyro;
+//Run this script with "DockOn", it'll set battery to "Recharge", thruster to "turn-off", beacon to "turn-off", gyro to "turn-off" and vice versa for argument "Dockoff";
+//If the type is like "IMyPowerProducer" or "IMyShipController", plz try to edit "setBlock" func directly.
 
-List<IMyThrust> ts = new List<IMyThrust>();
-List<IMyBatteryBlock> pwr = new List<IMyBatteryBlock>();
-List<IMyGasTank> gasTank = new List<IMyGasTank>();
-List<IMyTerminalBlock> inv = new List<IMyTerminalBlock>();
-List<IMyRadioAntenna> anT = new List<IMyRadioAntenna>();
-List<IMyBeacon> beaC = new List<IMyBeacon>();
-List<IMyPowerProducer> pwrProduc = new List<IMyPowerProducer>();
-List<IMyGasGenerator> gasGen = new List<IMyGasGenerator>();
-List<IMyGyro> gyRo = new List<IMyGyro>();
-List<IMyTerminalBlock> all = new List<IMyTerminalBlock>();
-IMyProgrammableBlock dockGroup;
+List<IMyTerminalBlock> allBlocks = new List<IMyTerminalBlock>();
 MyIni _ini = new MyIni();
 string errTxt = "";
-List<IMyShipConnector> otherConnect = new List<IMyShipConnector>();
-IMyShipConnector mainConnect;
+IMyCubeGrid operationGrid;
 
 public Program(){
-    _ini.Clear();
-    _ini.TryParse(Me.CustomData);
+    _ini.Clear(); _ini.TryParse(Me.CustomData); operationGrid = Me.CubeGrid;
     scriptDisplay("Grid Config");
-    setStat(true);
-    mainConnect = GridTerminalSystem.GetBlockWithId(NameToId("MainConnect", "Connector")) as IMyShipConnector;
-    dockGroup = GridTerminalSystem.GetBlockWithId(NameToId("MainConnect", "Connector")) as IMyProgrammableBlock;
-    if (mainConnect == null) { errTxt += "Need Main-connect name to prevent setting error."; return; }
-    List<IMyShipConnector> allConnect = new List<IMyShipConnector>();
-    GridTerminalSystem.GetBlocksOfType(allConnect);
-    foreach (var c in allConnect)
-    { if(c.CustomName != mainConnect.CustomName) { otherConnect.Add(c); } }
-    errTxt += "Run the script to initialize."; Echo(errTxt);
+    GridTerminalSystem.GetBlocksOfType(allBlocks);
+    errTxt += $"Operation grid's name:\n{operationGrid.CustomName}\nRun the script to initialize.\n"; Echo(errTxt);
 }
 
 public void Main(string argument) {
     errTxt = "";
-    if (argument == "") { errTxt += "Waiting for reconnect...\n"; foreach (var c in otherConnect) { c.Disconnect(); }  mainConnect.Disconnect(); TB.Trigger(); }
-    else if (argument == "Start")
-    {
-        _ini.Clear(); _ini.TryParse(Me.CustomData);
-        delKeys("Battery");
-        GridTerminalSystem.GetBlocksOfType(pwr);
-        if (pwr.Count != 0)
-        { foreach (var p in pwr) { _ini.Set("Battery", p.CustomName, p.EntityId); } }
-        delKeys("O2Tank");
-        GridTerminalSystem.GetBlocksOfType(gasTank);
-        if (gasTank.Count != 0)
-        { foreach (var o in gasTank) { if (o.BlockDefinition.SubtypeId.Contains("Oxygen") || o.BlockDefinition.SubtypeId == "") { _ini.Set("O2Tank", o.CustomName, o.EntityId); } } }
-        delKeys("H2Tank");
-        GridTerminalSystem.GetBlocksOfType(gasTank);
-        if (gasTank.Count != 0)
-        { foreach (var h in gasTank) { if (h.BlockDefinition.SubtypeId.Contains("Hydrogen")) { _ini.Set("H2Tank", h.CustomName, h.EntityId); } } }
-        delKeys("Thruster");
-        GridTerminalSystem.GetBlocksOfType(ts);
-        if (ts.Count != 0)
-        { foreach (var t in ts) { _ini.Set("Thruster", t.CustomName, t.EntityId); } }
-        delKeys("Inventory");
-        GridTerminalSystem.GetBlocksOfType(inv);
-        if (inv.Count != 0)
-        { foreach (var i in inv) { if (i.HasInventory) { _ini.Set("Inventory", i.CustomName, i.EntityId); } } }
-        delKeys("RadioAntenna");
-        GridTerminalSystem.GetBlocksOfType(anT);
-        if (anT.Count != 0)
-        { foreach (var a in anT) { _ini.Set("RadioAntenna", a.CustomName, a.EntityId); } }
-        delKeys("Beacon");
-        GridTerminalSystem.GetBlocksOfType(beaC);
-        if (beaC.Count != 0)
-        { foreach (var b in beaC) { _ini.Set("Beacon", b.CustomName, b.EntityId); } }
-        delKeys("PWRProducer");
-        GridTerminalSystem.GetBlocksOfType(pwrProduc);
-        if (pwrProduc.Count != 0)
-        { foreach (var p in pwrProduc) { if (!p.BlockDefinition.TypeIdString.Contains("Battery")) { _ini.Set("PWRProducer", p.CustomName, p.EntityId); } } }
-        delKeys("GasGenerator");
-        GridTerminalSystem.GetBlocksOfType(gasGen);
-        if (gasGen.Count != 0)
-        { foreach (var g in gasGen) { _ini.Set("GasGenerator", g.CustomName, g.EntityId); } }
-        delKeys("Gyro");
-        GridTerminalSystem.GetBlocksOfType(gyRo);
-        if (gyRo.Count != 0)
-        { foreach (var g in gyRo) { _ini.Set("Gyro", g.CustomName, g.EntityId); } }
-        Me.CustomData = _ini.ToString();
-        mainConnect.Connect();
-        foreach (var c in otherConnect) { c.Connect(); }
-        errTxt += "Initialization completed.";
+    if(argument == "") {
+        _ini.Clear(); Me.CustomData = ""; _ini.TryParse(Me.CustomData);
+        setBlocks(); Me.CustomData = _ini.ToString();
+        errTxt += $"{operationGrid.CustomName}\n(ID: {operationGrid.EntityId})\ninitialize completed.";
     }
     else if (argument == "DockOn")
     {
@@ -108,6 +41,30 @@ public void Main(string argument) {
     }
     Echo(errTxt);
 }
+
+void setBlocks() {
+    foreach (Type key in trans.Keys) {
+        string section = trans[key];
+        foreach (var block in allBlocks) {
+            if(block.CubeGrid.EntityId == operationGrid.EntityId)
+            {
+                block.ShowInToolbarConfig = false; block.ShowInTerminal = false;
+                string keyName = "I" + block.GetType().Name;
+                if (keyName == key.Name){
+                    if (key == typeof(IMyGasTank)){
+                        if (block.BlockDefinition.SubtypeId.Contains("Hydrogen")) { _ini.Set("H2Tank", illegalChk(block.CustomName), block.EntityId.ToString()); }
+                        else { _ini.Set("O2Tank", illegalChk(block.CustomName), block.EntityId.ToString()); }
+                    }
+                    else { _ini.Set(section, illegalChk(block.CustomName), block.EntityId.ToString()); }
+                }
+                if (block.HasInventory) { _ini.Set("Inventory", illegalChk(block.CustomName), block.EntityId.ToString()); }
+                if (block is IMyPowerProducer && !(block is IMyBatteryBlock)) { _ini.Set("Generator", illegalChk(block.CustomName), block.EntityId.ToString()); }
+            }
+        }
+    }
+}
+
+string illegalChk(string str) { return str.Replace("|", "").Replace(",", "").Replace("=", ""); }
 
 List<long> getBlocks(string section)
 {
@@ -141,25 +98,12 @@ long NameToId(string section, string key)
         {
             errTxt += $"Error: the {key} called {_ini.Get(section, key).ToString()} is not found.";
             Echo(errTxt + "\n");
+            return 0;
         }
         _ini.Set(section, key, entity.EntityId.ToString());
         Me.CustomData = _ini.ToString();
     }
     return entityId;
-}
-
-IMyTimerBlock TB;
-void setStat(bool i)
-{
-    if (i)
-    { _ini.TryParse(Me.CustomData); TB = GridTerminalSystem.GetBlockWithId(NameToId("RunningStat", "TB")) as IMyTimerBlock; }
-    else
-    {
-        if (TB == null)
-        { errTxt += "Timer block not found,\nmay not detect script's stat."; }
-        else
-        { TB.TriggerDelay = 1; TB.Enabled = true; TB.StartCountdown(); }
-    }
 }
 
 void scriptDisplay(string scriptTitle)
@@ -172,9 +116,11 @@ void scriptDisplay(string scriptTitle)
     Me.GetSurface(1).WriteText("\n" + scriptTitle);
 }
 
-void delKeys(string section)
-{
-    List<MyIniKey> keys = new List<MyIniKey>();
-    _ini.GetKeys(section, keys);
-    foreach (var key in keys) { _ini.Delete(key); }
-}
+Dictionary<Type, string> trans = new Dictionary<Type, string>() {
+    { typeof(IMyBatteryBlock), "Battery"},
+    { typeof(IMyThrust), "Thruster"},
+    { typeof(IMyRadioAntenna), "Antenna"},
+    { typeof(IMyBeacon), "Beacon"},
+    { typeof(IMyGyro), "Gyro"},
+    { typeof(IMyGasTank), "GasTank"},
+};
